7ee06460fb82b894f2594c4c9002c9c339e52436
VladimirEliseev <33568155+VladimirEliseev@users.noreply.github.com>
2019-10-12 22:10:29 +0300

    Update task-02.js

--- src/ex6_js-array-methods/task-02.js
+++ src/ex6_js-array-methods/task-02.js
@@ -1,13 +1,13 @@
 'use strict';
 function some(array, callback){
-	let item;
-	for(let i=0;i<array.length;i++){
-		item=array[i];
-		if(callback(item, i, array)){
-			return true
-		}
-	}
-	return false
+  let item;
+  for(let i=0;i<array.length;i++){
+    item=array[i];
+    if(callback(item, i, array)){
+      return true
+    }
+  }
+  return false
 }
 
 module.exports=some;
