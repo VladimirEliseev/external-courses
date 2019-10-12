bceb4d73d3b8cc111044c19b920b8a7e508fb9a2
VladimirEliseev <33568155+VladimirEliseev@users.noreply.github.com>
2019-10-12 22:09:25 +0300

    Update task-01.js

--- src/ex6_js-array-methods/task-01.js
+++ src/ex6_js-array-methods/task-01.js
@@ -1,19 +1,18 @@
 function slice(array, begin=0, end=array.length){
-	let newArray=[],index=0,newBegin=begin, newEnd=end;
-	if(begin<0 && end>=0){
-		newBegin+=array.length;
-	}else if(begin>=0 && end<0){
-		newEnd+=array.length;
-	}else if(begin<0 && end<0){
-		newBegin+=array.length;
-		newEnd+=array.length;
-	};
-
-	for(let i=newBegin;i<newEnd;i++){
-		newArray[index]=array[i];
-		index++;
-	}
-	return newArray
+  let newArray=[],index=0,newBegin=begin, newEnd=end;
+  if(begin<0 && end>=0){
+    newBegin+=array.length;
+  }else if(begin>=0 && end<0){
+    newEnd+=array.length;
+  }else if(begin<0 && end<0){
+    newBegin+=array.length;
+    newEnd+=array.length;
+  };
+  for(let i=newBegin;i<newEnd;i++){
+    newArray[index]=array[i];
+    index++;
+  }
+  return newArray
 }
 
 module.exports=slice;
