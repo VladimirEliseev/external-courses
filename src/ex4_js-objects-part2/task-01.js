2c0a086df689e5bee11b46c885e88b6d874185fa
VladimirEliseev <33568155+VladimirEliseev@users.noreply.github.com>
2019-10-12 21:55:20 +0300

    Update task-01.js

--- src/ex4_js-objects-part2/task-01.js
+++ src/ex4_js-objects-part2/task-01.js
@@ -1,10 +1,10 @@
 function findProp(prop,object){
-	for(let key in object){
-		if(!object.hasOwnProperty(key) && key===prop){
-	 		return object[prop]
-	 	}
-	}
-	return undefined
+  for(let key in object){
+    if(!object.hasOwnProperty(key) && key===prop){
+      return object[prop]
+    }
+  }
+  return undefined
 }
 
 module.exports=findProp;
