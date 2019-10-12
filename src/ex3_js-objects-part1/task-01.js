ec8a251fa515c37264641f4260319aa766976ac1
VladimirEliseev <33568155+VladimirEliseev@users.noreply.github.com>
2019-10-12 21:49:15 +0300

    Update task-01.js

--- src/ex3_js-objects-part1/task-01.js
+++ src/ex3_js-objects-part1/task-01.js
@@ -1,8 +1,8 @@
 function createObject(){
-	const person={};
-	person.name='Andrew';
-	person.age=20;
-	person.married=false;
+  const person={};
+  person.name='Andrew';
+  person.age=20;
+  person.married=false;
   delete person.married;
 }
 
