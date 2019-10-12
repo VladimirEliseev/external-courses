6ee0b32d7721ccbc706acd799c8aa5e5a9c71d60
VladimirEliseev <33568155+VladimirEliseev@users.noreply.github.com>
2019-10-12 22:14:31 +0300

    Update task-05.js

--- src/ex6_js-array-methods/task-05.js
+++ src/ex6_js-array-methods/task-05.js
@@ -1,12 +1,12 @@
 'use strict';
 function map(array, callback){
-	let item;
-	const newArray=[];
-	for(let i=0;i<array.length;i++){
-		item=array[i];
-		newArray[i]=callback(item,i,array);
-	}
-	return newArray
+  let item;
+  const newArray=[];
+  for(let i=0;i<array.length;i++){
+    item=array[i];
+    newArray[i]=callback(item,i,array);
+  }
+  return newArray
 }
 
 module.exports=map;
