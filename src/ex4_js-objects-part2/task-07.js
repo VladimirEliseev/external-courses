b413e7fcb49c4bf348ff743a414a7474e5eb2f6e
VladimirEliseev <33568155+VladimirEliseev@users.noreply.github.com>
2019-10-12 22:00:15 +0300

    Update task-07.js

--- src/ex4_js-objects-part2/task-07.js
+++ src/ex4_js-objects-part2/task-07.js
@@ -1,7 +1,7 @@
 function getLimitString(string,number){
-	let newString;
+  let newString;
   if(string.length>number){
-   	newString=string.substr(0,number-1)+'…';
+     newString=string.substr(0,number-1)+'…';
   }
   return newString
 }
