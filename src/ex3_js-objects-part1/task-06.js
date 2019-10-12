deec72649f004c2543b5e3a3f344aec7008fc3c0
VladimirEliseev <33568155+VladimirEliseev@users.noreply.github.com>
2019-10-12 21:53:39 +0300

    Update task-06.js

--- src/ex3_js-objects-part1/task-06.js
+++ src/ex3_js-objects-part1/task-06.js
@@ -1,10 +1,10 @@
 function createDeepCopyObject(object) {
-	let copyObject;  
-	if(Array.isArray(object)){
-		copyObject=[];
-	}else{
-		copyObject={};
-	}
+  let copyObject;  
+  if(Array.isArray(object)){
+    copyObject=[];
+  }else{
+    copyObject={};
+  }
   for (let key in object){
     if (typeof object[key] === "object") {
       copyObject[key]=createDeepCopyObject(object[key]);
