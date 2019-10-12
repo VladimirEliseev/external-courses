b78455330febaf3e5ba2939aa1884df0b6354ac5
VladimirEliseev <33568155+VladimirEliseev@users.noreply.github.com>
2019-10-12 22:12:45 +0300

    Update task-03.js

--- src/ex6_js-array-methods/task-03.js
+++ src/ex6_js-array-methods/task-03.js
@@ -1,13 +1,13 @@
 'use strict';
 function every(array, callback){
-	let item;
-	for(let i=0;i<array.length;i++){
-		item=array[i];
-		if(!callback(item, i, array)){
-			return false
-		}
-	}
-	return true
+  let item;
+  for(let i=0;i<array.length;i++){
+    item=array[i];
+    if(!callback(item, i, array)){
+      return false
+     }
+   }
+  return true
 }
 
 module.exports=every;
