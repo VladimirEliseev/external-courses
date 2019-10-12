acd5a6efde99d9a2a5720c70830e49f7ff44e2cc
VladimirEliseev <33568155+VladimirEliseev@users.noreply.github.com>
2019-10-12 22:13:58 +0300

    Update task-04.js

--- src/ex6_js-array-methods/task-04.js
+++ src/ex6_js-array-methods/task-04.js
@@ -1,14 +1,14 @@
 'use strict';
 function filter(array, callback){
-	let item;
-	const newArray=[];
-	for(let i=0;i<array.length;i++){
-		item=array[i];
-		if(callback(item, i, array)){
-			newArray[newArray.length]=item;
-		}
-	}
-	return newArray
+  let item;
+  const newArray=[];
+  for(let i=0;i<array.length;i++){
+    item=array[i];
+    if(callback(item, i, array)){
+      newArray[newArray.length]=item;
+    }
+  }
+  return newArray
 }
 
 module.exports=filter;
