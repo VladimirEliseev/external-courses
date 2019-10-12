10fb74bead7623d0943a15188e37505ea2c5babd
VladimirEliseev <33568155+VladimirEliseev@users.noreply.github.com>
2019-10-12 21:58:41 +0300

    Update task-04.js

--- src/ex4_js-objects-part2/task-04.js
+++ src/ex4_js-objects-part2/task-04.js
@@ -1,8 +1,8 @@
 function changeFirstSymb(string){
-	let newString=string[0].toUpperCase();
-	let remainString=string.substr(1,string.length-1);
-	newString+=remainString;
-	return newString;
+  let newString=string[0].toUpperCase();
+  let remainString=string.substr(1,string.length-1);
+  newString+=remainString;
+  return newString;
 }
 
 module.exports=changeFirstSymb;
