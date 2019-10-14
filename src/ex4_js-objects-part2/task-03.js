847d2f5872f79db90f75008f61aa275fd7705044
VladimirEliseev <xxlxxl51@gmail.com>
2019-10-14 10:42:12 +0300

    Update task 02,03

--- src/ex4_js-objects-part2/task-03.js
+++ src/ex4_js-objects-part2/task-03.js
@@ -1,11 +1,15 @@
 function deleteSpace(string){
-  let newString;
-  let firstSpace=string.indexOf(' ');
-  let lastSpace=string.lastIndexOf(' ');
-  newString=string.substr(0,firstSpace);
-  newString+=string.substr(firstSpace+1, lastSpace-firstSpace-1);
-  newString+=string.substr(lastSpace+1,string.length-lastSpace-1);
-  return newString
+	let newString;
+  if(string[0]===' ' && string[string.length-1]===' '){
+    newString=string.substr(1,string.length-2)
+  }else if (string[0]===' '){
+    newString=string.substr(1,string.length-1)
+  }else if(string[string.length-1]===' '){
+    newString=string.substr(0,string.length-2)
+  }else{
+    newString=string;
+  }
+	return newString
 }
 
 module.exports=deleteSpace;
