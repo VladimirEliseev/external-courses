c31edda62f3463d03c974e15d3ac5ab0292dc1a1
VladimirEliseev <33568155+VladimirEliseev@users.noreply.github.com>
2019-10-14 22:23:29 +0300

    Update task.js

--- src/ex5_js-functions/task.js
+++ src/ex5_js-functions/task.js
@@ -1,50 +1,35 @@
 const calculator=(function(){
-	let currentState=0;
-	let start=true;
-	const add=function(value=0){
+  let currentState=0;
+	
+  const add=function(value=0){
     currentState+=value;
-   	return add
-   }
-
+    return add
+}
+  
   const subtract=function(value=0){
-  	if(start){
-  		currentState=value;
-  		start=false;
-  	}else{
     currentState-=value;
-    }
-   	return subtract
+    return subtract
   }
 
-  const divide=function(value=0){
-   	if(start){
-    	currentState=value;
-    	start=false;
-    }else if(value!==0){
-    	currentState/=value;
-    }
-   	return divide
+  const divide=function(value=1){
+    currentState/=value;
+    return divide
   }
 
-  const multiply=function(value=0){
-   	if(start){
-   		currentState=value;
-   		start=false;
-   	}else{
-    	currentState*=value
-    }
-   	return multiply
-  }	
+  const multiply=function(value=1){
+    currentState*=value
+    return multiply
+  }
 
   const getResult=function(){
-   	return currentState
+    return currentState
   }
 
   const reset=function(){
     currentState=0;
     return currentState
   }
-	return {add,subtract,divide,multiply, getResult,reset}
+  return {add,subtract,divide,multiply, getResult, reset}
 }());
 
 module.exports=calculator;
