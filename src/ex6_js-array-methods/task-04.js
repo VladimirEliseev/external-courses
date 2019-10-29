7ff0242e5636470b6fe766ebee4ccf4b55679045
VladimirEliseev <xxlxxl51@gmail.com>
2019-10-29 10:36:27 +0300

    Change task, deleted extra variable

--- src/ex6_js-array-methods/task-04.js
+++ src/ex6_js-array-methods/task-04.js
@@ -3,7 +3,7 @@ function filter(array, callback){
   const newArray=[];
   for(let i=0;i<array.length;i++){
     if(callback(array[i], i, array)){
-      newArray[newArray.length]=item;
+      newArray[newArray.length]=array[i];
     }
   }
   return newArray
