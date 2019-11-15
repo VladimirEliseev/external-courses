17a03dc989a702f6fe60833f81195bc180c888cf
VladimirEliseev <xxlxxl51@gmail.com>
2019-11-15 22:36:22 +0300

    Update

--- src/ex12_js_dom/task-01.js
+++ src/ex12_js_dom/task-01.js
@@ -1,79 +1,77 @@
-const arrayNumber=["asset/image1.jpg",
+const arrayPictures=["asset/image1.jpg",
   "asset/image2.jpg",
   "asset/image3.jpg"];
-
-let image1=document.querySelector(".image");
+let imageCurrent=document.querySelector(".image");
 let conteiner=document.querySelector(".conteiner-image");
 let imageNext=document.querySelector(".imageNext");
 let imageBack=document.querySelector(".imageBack");
-let buttonNext=document.querySelector('.buttonNext');
-let buttonBack=document.querySelector('.buttonBack');
-let image2=document.createElement('img');
-image2.className='image';
-conteiner.append(image2);
-image2.style.transform="scale(0)";
+const buttonNext=document.querySelector('.buttonNext');
+const buttonBack=document.querySelector('.buttonBack');
+let imageCurrentSub=document.createElement('img');
+imageCurrentSub.className='image';
+conteiner.append(imageCurrentSub);
+imageCurrentSub.style.transform="scale(0)";
 
-let key=0,currentNumber=0,img;
+let index=0,currentNumber=0,img;
 function animation(){
-  if(key===0){
-    img=image2;
-    image1.style.zIndex='1';
-    image2.style.zIndex='0';
-    image1.style.transform="scale(0)";
-    image2.style.transform="scale(1)";
-    image2.style.display='block';
-    key++;
+  if(index===0){
+    img=imageCurrentSub;
+    imageCurrent.style.zIndex='1';
+    imageCurrentSub.style.zIndex='0';
+    imageCurrent.style.transform="scale(0)";
+    imageCurrentSub.style.transform="scale(1)";
+    index++;
   }else{
-    img=image1;
-    image1.style.zIndex='0';
-    image2.style.zIndex='1';
-    image2.style.transform="scale(0)";
-    image1.style.transform="scale(1)";
-    key=0;
+    img=imageCurrent;
+    imageCurrent.style.zIndex='0';
+    imageCurrentSub.style.zIndex='1';
+    imageCurrentSub.style.transform="scale(0)";
+    imageCurrent.style.transform="scale(1)";
+    index=0;
   }
 }
 
-function next(){
+function goNext(){
   animation();
   currentNumber++;
-  if(currentNumber===arrayNumber.length-1){
-    img.src=arrayNumber[currentNumber];
-    imageNext.src=arrayNumber[0];
-    imageBack.src=arrayNumber[currentNumber-1];
+  if(currentNumber===arrayPictures.length-1){
+    img.src=arrayPictures[currentNumber];
+    imageNext.src=arrayPictures[0];
+    imageBack.src=arrayPictures[currentNumber-1];
   }
-  else if(currentNumber>arrayNumber.length-1){
+  else if(currentNumber>arrayPictures.length-1){
     currentNumber=0;
-    img.src=arrayNumber[currentNumber];
-    imageNext.src=arrayNumber[currentNumber+1];
-    imageBack.src=arrayNumber[arrayNumber.length-1];
+    img.src=arrayPictures[currentNumber];
+    imageNext.src=arrayPictures[currentNumber+1];
+    imageBack.src=arrayPictures[arrayPictures.length-1];
     
   }else{
-    img.src=arrayNumber[currentNumber];
-    imageNext.src=arrayNumber[currentNumber+1];
-    imageBack.src=arrayNumber[currentNumber-1];
+    img.src=arrayPictures[currentNumber];
+    imageNext.src=arrayPictures[currentNumber+1];
+    imageBack.src=arrayPictures[currentNumber-1];
   }
 }
 
-function back(){
+function goBack(){
   animation();
   currentNumber--;
   if(currentNumber<0){
-    currentNumber=arrayNumber.length-1;
-    img.src=arrayNumber[currentNumber]
-    imageNext.src=arrayNumber[0];
-    imageBack.src=arrayNumber[currentNumber-1];
+    currentNumber=arrayPictures.length-1;
+    img.src=arrayPictures[currentNumber];
+    imageNext.src=arrayPictures[0];
+    imageBack.src=arrayPictures[currentNumber-1];
   }
   if(currentNumber===0){
-    img.src=arrayNumber[currentNumber];
-    imageNext.src=arrayNumber[1];
-    imageBack.src=arrayNumber[arrayNumber.length-1];
+    img.src=arrayPictures[currentNumber];
+    imageNext.src=arrayPictures[1];
+    imageBack.src=arrayPictures[arrayPictures.length-1];
   }
   else{
-    img.src=arrayNumber[currentNumber];
-    imageNext.src=arrayNumber[currentNumber+1];
-    imageBack.src=arrayNumber[currentNumber-1];
+    img.src=arrayPictures[currentNumber];
+    imageNext.src=arrayPictures[currentNumber+1];
+    imageBack.src=arrayPictures[currentNumber-1];
   }
 }
 
-buttonNext.addEventListener('click',next);
-buttonBack.addEventListener('click',back);
+buttonNext.addEventListener('click',goNext);
+buttonBack.addEventListener('click',goBack);
--- src/ex12_js_dom/task-02.js
+++ src/ex12_js_dom/task-02.js
@@ -1,36 +1,35 @@
 let arrow=document.querySelector('.arrow');
 let blockPersonalInfo=document.querySelector('.personalInfo');
-let onDropDown=false;
+let isOpenMenu=false;
 
-function changeDropDown(){
-  if(!onDropDown){
+function openDropDown(){
+  if(!isOpenMenu){
     arrow.style.transform='rotate(180deg)';
-    onDropDown=true;
-    openDropDown();
+    isOpenMenu=true;
+    createDropDown();
   }else{
     arrow.style.transform='rotate(360deg)';
-    onDropDown=false;
-    closeDropDown();
+    isOpenMenu=false;
+    deleteDropDown();
   } 
 }
 
-function openDropDown(){
+function createDropDown(){
   let blockDropDown=document.createElement('div');
   blockDropDown.className='dropDown';
   blockPersonalInfo.append(blockDropDown);
-
   blockDropDown.append(document.createElement('button'));
   blockDropDown.append(document.createElement('button'));
   blockDropDown.append(document.createElement('button'));
-  let arrayLIst=blockDropDown.children;
-  arrayLIst[0].innerHTML='My account';
-  arrayLIst[1].innerHTML='My tasks';
-  arrayLIst[2].innerHTML='Log out';
+  let menuItems=blockDropDown.children;
+  menuItems[0].innerHTML='My account';
+  menuItems[1].innerHTML='My tasks';
+  menuItems[2].innerHTML='Log out';
 }
 
-function closeDropDown(){
-    let delDropDown=document.querySelector('.dropDown')
-    delDropDown.parentNode.removeChild(delDropDown);
+function deleteDropDown(){
+  let delDropDown=document.querySelector('.dropDown')
+  delDropDown.parentNode.removeChild(delDropDown);
 }
 
-arrow.addEventListener('click',changeDropDown);
+arrow.addEventListener('click',openDropDown);
